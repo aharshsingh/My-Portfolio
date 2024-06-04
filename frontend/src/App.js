8dd439c3fc7250dddb05c902f12227f292a2560b
aharshsingh <aharshsingh25@gmail.com>
2024-06-05 01:46:30 +0530

    Contact Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Landing from "./pages/Landing";
 import About from './pages/About'
 import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <div className="App">
-      <Portfolio/>
+      <Contact/>
+      {/* <Portfolio/> */}
       {/* <Landing/>
       <About/> */}
     </div>
---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import phone from '../public/phone-solid.svg'
+import '../css/contact.css'
+import email from '../public/envelope-solid.svg'
+import address from '../public/address-book-solid.svg'
+import twitter from '../public/twitter.svg'
+import linkedin from '../public/linkedin.svg'       
+import github from '../public/github.svg'
+import facebook from '../public/facebook.svg'
+import instagram from '../public/instagram.svg'
+export default function Contact() {
+  return (
+    <>
+    <div style={{display: 'flex', columnGap: '10px', marginLeft: '740px', marginTop: '50px'}}>
+        <h1 className='resume'>GET IN</h1>
+        <h2 className='aboutme'>TOUCH</h2>
+    </div>
+    <div style={{display:'flex', marginTop:'200px', marginLeft: '480px'}}>
+        <div style={{marginTop: '40px'}}>
+            <h2 style={{fontSize:'30px',color: '#FFB400'}}>DON'T BE SHY!</h2>
+            <p style={{fontSize: '20px', color:'#666666'}}>Feel free to get in touch with me. I am always<br/> open to discussing new projects, creative ideas or<br/> opportunities to be part of your visions.</p>
+        </div>
+        <div style={{borderRight: '1px solid #666666', height:'200px', marginLeft:'100px', opacity:'0.5'}}></div>
+        <div style={{marginLeft: '100px' , marginTop: '0px'}}>
+        <div style={{display: 'flex', columnGap: '20px'}}>
+            <div className='circleContainercontact'>
+            <img style={{height:'30px', width:'30px', position:'absolute', left:'10px', top:'10px'}} src={phone} alt='logo' />
+            </div>
+            <p style={{fontSize:'20px', color:'#666666'}}>Phone: <br/>+91-8005198273</p>
+        </div>
+        <div style={{display: 'flex', columnGap: '20px', marginTop:'20px'}}>
+            <div className='circleContainercontact'>
+            <img style={{height:'30px', width:'30px', position:'absolute', left:'10px', top:'10px'}} src={email} alt='logo' />
+            </div>
+            <p style={{fontSize:'20px', color:'#666666'}}>Email: <br/>aharshsingh25@gmail.com</p>
+        </div>
+        <div style={{display: 'flex', columnGap: '20px', marginTop:'20px'}}>
+            <div className='circleContainercontact'>
+            <img style={{height:'30px', width:'30px', position:'absolute', left:'10px', top:'10px'}} src={address} alt='logo' />
+            </div>
+            <p style={{fontSize:'20px', color:'#666666'}}>Address: <br/>Ayodhya, Uttar Pradesh, India</p>
+        </div>
+        </div>
+    </div>
+    <div style={{display:'flex', columnGap:'20px', marginTop: '50px', marginLeft:'850px'}}>
+        <div className='circleContainercontact1'>
+            <img style={{height:'20px', width:'20px', position:'absolute', left:'10px', top:'10px'}} src={facebook} alt='logo' />
+        </div>
+        <div className='circleContainercontact1'>
+            <img style={{height:'20px', width:'20px', position:'absolute', left:'10px', top:'10px'}} src={instagram} alt='logo' />
+        </div>
+        <div className='circleContainercontact1'>
+            <img style={{height:'20px', width:'20px', position:'absolute', left:'10px', top:'10px'}} src={twitter} alt='logo' />
+        </div>
+        <div className='circleContainercontact1'>
+            <img style={{height:'20px', width:'20px', position:'absolute', left:'10px', top:'10px'}} src={linkedin} alt='logo' />
+        </div>
+        <div className='circleContainercontact1'>
+            <img style={{height:'20px', width:'20px', position:'absolute', left:'10px', top:'10px'}} src={github} alt='logo' />
+        </div>
+    </div>
+    </>
+  )
+}
