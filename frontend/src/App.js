401eb7bbb9b29929f9a993458a7c4eb24b7315b7
aharshsingh <aharshsingh25@gmail.com>
2024-06-05 00:14:56 +0530

    Portfolio Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Landing from "./pages/Landing";
 import About from './pages/About'
+import Portfolio from "./pages/Portfolio";
 function App() {
   return (
     <div className="App">
-
-      <About/>
+      <Portfolio/>
+      {/* <Landing/>
+      <About/> */}
     </div>
   );
 }
---
+++ frontend/src/pages/Portfolio.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../css/Portfolio.css';
+import fashion from '../public/Screenshot 2024-06-03 031822.png';                                                                                                                                                 
+export default function Portfolio() {
+  return (
+    <>
+    <div style={{display: 'flex', columnGap: '10px', marginLeft: '740px', marginTop: '50px'}}>
+        <h1 className='resume'>MY</h1>
+        <h2 className='aboutme'>PORTFOLIO</h2>
+    </div>
+    <div style={{display:'flex', marginLeft: '120px', marginTop: '100px'}}>
+        <img className='imageProject' src={fashion} alt='imageProject' />
+        <img className='imageProject' src={fashion} alt='imageProject' />
+        <img className='imageProject' src={fashion} alt='imageProject' />
+        <img className='imageProject' src={fashion} alt='imageProject' />
+    </div>
+    <div style={{display:'flex', marginLeft: '120px', marginTop: '10px'}}>
+        <img className='imageProject' src={fashion} alt='imageProject' />
+        <img className='imageProject' src={fashion} alt='imageProject' />
+        <img className='imageProject' src={fashion} alt='imageProject' />
+        <img className='imageProject' src={fashion} alt='imageProject' />
+    </div>
+    <div style={{display: 'flex', columnGap: '10px', marginLeft: '540px', marginTop: '190px'}}>
+        <h1 className='resume'>CO-CURRICUALAR</h1>
+        <h2 className='aboutme'>ACTIVITY</h2>
+    </div>
+    <div style={{display:'flex', marginTop: '100px', marginLeft:'450px'}}>
+        <div style={{boxShadow:'3px 3px 3px #D9D9D9', width: '300px',height: '150px',borderRadius: '10px', border: '0px', marginRight:'50px'}} className='card'>
+            <p style={{fontSize: '40px', paddingLeft: '10px', color: '#FFB400'}}>1 YEAR+</p>
+            <p style={{marginTop: '20px', fontSize: '20px', paddingLeft: '10px',color: '#666666'}}>Head of Department in Student Organization | Coding NinjasLPU </p>
+        </div>
+        <div style={{boxShadow:'3px 3px 3px #D9D9D9', width: '300px',height: '150px',borderRadius: '10px', border: '0px',marginRight:'50px'}} className='card'>
+            <p style={{fontSize: '40px', paddingLeft: '10px', color: '#FFB400'}}>Guest Lecture</p>
+            <p style={{marginTop: '20px', fontSize: '20px', paddingLeft: '10px',color: '#666666'}}>On AI powered WebD | Student Organization Zillion Team L&T</p>
+        </div>
+        <div style={{boxShadow:'3px 3px 3px #D9D9D9', width: '300px',height: '150px',borderRadius: '10px', border: '0px',marginRight:'50px'}} className='card'>
+            <p style={{fontSize: '40px', paddingLeft: '10px', color: '#FFB400'}}>BLOGS</p>
+            <p style={{marginTop: '20px', fontSize: '20px', paddingLeft: '10px',color: '#666666'}}>TechWeekly Blogs<span style={{cursor:'pointer'}}><br/>Check Here</span></p>
+        </div>
+      </div>
+      <div style={{marginTop:'200px'}}></div>
+    </>
+  )
+}
