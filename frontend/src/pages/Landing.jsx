e7ecb708f7509ff3f3901f6a3c7c1e6374e35368
aharshsingh <aharshsingh25@gmail.com>
2025-02-25 10:41:38 +0530

    responsive

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -12,14 +12,24 @@ export default function Landing() {
     <div>
       <Navbar/>
     </div>
-      <div className='flex lg:justify-between flex-wrap'>
+      <div className='lg:flex lg:justify-between lg:flex-wrap'>
         <div className="hidden lg:block">
           <img src={homeImage} alt='homeImage' className="h-full w-[600px]"/>
         </div>
-        <div className='lg:mt-72 flex flex-row justify-between mt-12'>
-          <div>
-            <img className='lg:hidden block rounded-full w-32 ml-2' src={me} alt='me'/>
+        <div className='lg:mt-72 mt-12'>
+          <div className='flex justify-center items-center'>
+            <Link to='/about'><div className="relative w-52 ml-2 rounded-full overflow-hidden group">
+              <img
+                className="lg:hidden block rounded-full w-52 transition-all duration-300 group-hover:brightness-50"
+                src={me}
+                alt="me"
+              />
+              <span className="absolute inset-0 flex items-center justify-center text-white font-semibold opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                About Me
+              </span>
+            </div></Link>
           </div>
+          <div className='flex justify-center items-center mt-10'>
           <div>
           <div className='lg:w-[500px] text-left font-bold text-md text-transparent bg-clip-text bg-gradient-to-r from-[#ffb400] via-[#ffc41d] via-[#ffd332] via-[#ffe345] to-[#ff912a] lg:text-5xl'>
             <Typewriter
@@ -29,7 +39,6 @@ export default function Landing() {
                         loop: true,
                         pauseFor: 2000,
                       }}
-                      
                     />
           </div>
           <p className=' text-md lg:text-5xl text-[#666666] font-bold'>WEB DEVELOPER</p>
@@ -44,8 +53,9 @@ export default function Landing() {
               those around me.</p>
               </div>
         </div>
-        <div className='lg:mt-96 lg:mr-40 mt-28 ml-28'>
-          <Link to='/Portfolio'><button className='rounded-full bg-[#FFB400] lg:bg-white text-white w-[200px] border-[#FFB400] border-[1px] p-3 lg:text-[#666666] font-semibold text-lg hover:bg-[#FFB400] hover:text-white transition-colors duration-300 ease-in-out flex justify-around '>Projects<span><img className='w-14 h-8' src={rightArrow} alt='arrow' /></span></button></Link>
+        </div>
+        <div className=' lg:mr-40 lg:mt-28 mt-10 flex justify-center items-center'>
+          <Link to='/Portfolio'><button className='rounded-full bg-[#FFB400] lg:bg-white text-white lg:w-[200px] w-[150px] border-[#FFB400] border-[1px] lg:p-3 p-2  lg:text-[#666666] font-semibold text-lg hover:bg-[#FFB400] hover:text-white transition-colors duration-300 ease-in-out flex justify-around lg:pl-0 pl-4'>Projects<span><img className='lg:w-14 w-12 lg:h-8 h-5 lg:mt-0 mt-1' src={rightArrow} alt='arrow' /></span></button></Link>
         </div>
       </div>
     </>
